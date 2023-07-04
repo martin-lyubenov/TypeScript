6a2ce1f36000aefcc722b111395fe2dedc658164
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-04 17:13:46 +0300

    first project - end

---
+++ Basics/using-ts.js
@@ -0,0 +1,9 @@
+var button = document.querySelector("button");
+var input1 = document.querySelector("#num1");
+var input2 = document.querySelector("#num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(Number(input1.valu), Number(input2.value)));
+});
